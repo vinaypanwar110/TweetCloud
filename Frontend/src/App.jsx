024cfe18b4b87ab611a941af56c06e863e61423e
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-07-26 13:36:39 +0530

    Added NotificationPage , ProfilePage, EditProfilePage and Skeletons

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import  SignUpPage  from "./pages/auth/signup/SignUpPage";
 import  LoginPage  from "./pages/auth/login/LoginPage";
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
       <RightPanel/>
     </div>
