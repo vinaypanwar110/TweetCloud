731e8c64d1e1cf0c97ba5d6f54e85aa1e70557ee
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-07-27 17:03:11 +0530

    Use react Query, Auth and protect routes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port:3000
+    ,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   }
+  ,
+
 })
