2abd0725ffdce41df4d76b631576d8a9decd5aaf
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-07-29 17:44:45 +0530

    Added versel.json

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt||req.headers.authorization?.split(" ")[1];;
 
     if (!token) {
       return res.status(401).json({ error: "Unauthorized: No Token Provided" });
