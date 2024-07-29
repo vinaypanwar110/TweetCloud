3b46836ff2f175fdff1ce29f9fe971e296a35f6f
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-07-29 19:14:14 +0530

    Another deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -21,6 +22,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;  
 
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json({ limit: "5mb" })); // working b/w req and respond // to parse req.body// limit upto 5mb to accept the data from the client
 app.use(express.urlencoded({ extended: true })); // to parse form data;
@@ -31,6 +34,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   console.log(`server is running at ${PORT}`);
